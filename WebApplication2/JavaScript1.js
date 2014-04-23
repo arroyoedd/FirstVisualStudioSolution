31fe0604fa9b57aceb785c645a1996980c899cd6
arroyoedd <arroyoedd@hotmail.com>
2014-04-23 17:26:07 -0500

    Todo list updates

--- WebApplication2/JavaScript1.js
+++ WebApplication2/JavaScript1.js
@@ -1,7 +1,42 @@
-﻿"use string";
-var shoot = "hi its ed money";
-var length = 4;
-for (var i = 0; i < length; i++) {
-    shoot = i;
-}
-alert(shoot);
+﻿"use strict";
+
+var toDoCounter = 0;
+
+var elementDoSomething = function(){
+    "use strict";
+    toDoCounter++;
+    var idElement = [];
+
+    var inputValue = document.getElementById("todoInput");
+    var outputOrderedList = document.getElementById("todoUnorderedList");
+    idElement.push('<li id="item' + toDoCounter + '" onclick="strikeOutListItem(\'' + 'item' + toDoCounter + '\');">' + inputValue.value + '<div onclick="deleteItem(' + 'item' + toDoCounter + ');"> delete me</div>' + '</li>');
+
+    for (var i = 0; i < idElement.length; i++) {
+        outputOrderedList.innerHTML += idElement[i];
+    }
+
+    //idElement.push('item' + toDoCounter);
+
+    //outputOrderedList.innerHTML += '<li id="item' + toDoCounter + '" onclick="strikeOutListItem(\''+'item' + toDoCounter + '\');">' + inputValue.value + '<div onclick="deleteItem('+ 'item' + toDoCounter +');"> delete me</div>' + '</li>';
+
+
+   
+};
+   
+var deleteItem = function (clicker) {
+    clicker.style.display = 'none';
+};
+var strikeOutListItem = function (listItemId) {
+    "use strict";
+    var x = document.getElementById(listItemId);
+    x.setAttribute("class", "strikethrough");
+    //if (x.style.textDecorationLineThrough == "line-through") {
+      //  x.style.display = 'none';
+    //}
+
+};
+
+
+
+//write a strikethrough if clicked for each li
+//add a new list element
